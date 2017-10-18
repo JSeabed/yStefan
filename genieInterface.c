dc78b5eb04755cb791bb57a01dc17010ed21d60d
Jeroen <jeroen@seabed.nl>
2017-10-18 16:48:41 +0200

    Update genieInterface.c

--- genieInterface.c
+++ genieInterface.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h> // for usleep
 #include <geniePi.h>
 
 #define GENIE_OBJ_FORM 10
+#define GENIE_OBJ_USERBUTTON 33
+#define GENIE_OBJ_4DBUTTON 30
+
+#define FROM(x) (0x010a + x + 0000)
+
+/*#define checksum(x) (x ^= x)*/
 
 /*
    ip
@@ -33,59 +40,66 @@
    gpgga
    ins
    */
-
+/*
 typedef int bool;
 #define true 1
 #define false 0
 
 struct data{
 	char* ip; 
-	float[] gpgga;
+	float gpgga;
 	bool ins_active; 
 	bool ins_aligning; 
-	bool ins_high_variance
-	bool ins_solution_good
-	bool ins_solution_free
-	bool ins_alignment_complete
-	bool determining_orientation
-	bool waiting_initialpos
-	bool waiting_azimuth
-	bool initializing_biases
-	bool motion_detect
-	bool finesteering
-	bool coarsesteering
-	bool unknown
-	bool aproximate
-	bool coarseadjusting
-	bool coarse
-	bool freewheeling
-	bool fineadjusting
-	bool fine
-	bool finebackupsteering
-	bool sattime
-	bool gpgga
-	bool ins
+	bool ins_high_variance;
+	bool ins_solution_good;
+	bool ins_solution_free;
+	bool ins_alignment_complete;
+	bool determining_orientation;
+	bool waiting_initialpos;
+	bool waiting_azimuth;
+	bool initializing_biases;
+	bool motion_detect;
+	bool finesteering;
+	bool coarsesteering;
+	bool unknown;
+	bool aproximate;
+	bool coarseadjusting;
+	bool coarse;
+	bool freewheeling;
+	bool fineadjusting;
+	bool fine;
+	bool finebackupsteering;
+	bool sattime;
+	bool ins;
 };
-
+*/
+/*
+	if(reply->object == GENIE_OBJ_4DBUTTON) {
+*/
 
 void handleEvent (struct genieReplyStruct *reply) {
-	if(reply->object == GENIE_OBJ_FORM) {
+if(reply->object == GENIE_OBJ_4DBUTTON) {
     switch (reply->index) {
       case 0:
 	/* Main screen. Show no data. Save data.*/
-        genieWriteStr(0,"You pressed the RED button.");
+        genieWriteStr(1,"You pressed the RED button.");
+        genieWriteObj(GENIE_OBJ_FORM, 1, 1);
 	printf("RED");
+	printf("%d\n");
         break;
       case 1:
 	/* Screen with data. Obtain old data? */
-        genieWriteStr(0,"You pressed the GREEN button.");
+        genieWriteStr(2,"You pressed the GREEN button.");
+        genieWriteObj(GENIE_OBJ_FORM,0, 1);
 	printf("Green");
+	printf("%d\n");
         break;
      default:
-	printf("Error, index not in range or found.")
+	printf("Error, index not in range or found.");
+	exit(0);
 	break;
     }
-  }
+    }
 }
 
 void getData(void){
@@ -95,7 +109,7 @@ void getData(void){
 int main (int argc, char** argv) {
   struct genieReplyStruct reply;
 
-  if(genieSetup("/dev/ttyAMA0",115200)<0) {
+  if(genieSetup("/dev/ttyAMA0",9600)<0) {
     printf("ViSi-Genie Failed to init display!\r\n");
     return(1); // Failed to initialize ViSi-Genie Display. Check Connections!
   }
@@ -104,7 +118,6 @@ usleep(20000);
     while(genieReplyAvail()) {
       genieGetReply(&reply);
       handleEvent(&reply);
-      printf("Ik kom hier");
       usleep(20000); // wait 20ms between polls to save CPU
     }
   }
