a06b6989050c72b51009827acf9c00b7154ac57c
stefan <512683@student.inholland.nl>
2017-10-26 16:58:34 +0200

    fix

--- log.py
+++ log.py
@@ -1,13 +1,32 @@
 import logging
 import sys
 
+def getLogger():
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    #logger.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+
+    # create a file handler
+    handler = logging.FileHandler('python.log')
+    handler.setLevel(logging.INFO)
+
+    # create a logging format
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+
+    # add the handlers to the logger
+    logger.addHandler(handler)
+
+    return logger
 
 def setup_custom_logger(name):
-    formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(module)s - %(message)s')
 
     handler = logging.StreamHandler()
-    handler.setFormatter(formatter)
+
     handler = logging.FileHandler('todo.log')
+
+    formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(module)s - %(message)s')
+    handler.setFormatter(formatter)
     handler.setLevel(logging.INFO)
 
     logger = logging.getLogger(name)
