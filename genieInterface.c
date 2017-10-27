1df25757bfe5186ff4d2c828200331de8475415c
Jeroen <jeroen@seabed.nl>
2017-10-27 14:32:33 +0200

    ffflusherino

--- genieInterface.c
+++ genieInterface.c
@@ -218,7 +218,7 @@ int main (int argc, char** argv) {
 			read(fd_parent[0], &readBuffer, BUFFSIZE);
 			printf("\n parent: %s", readBuffer);
 			genieWriteStr(1, readBuffer);
-			fflush(stdout);
+			fflush(fd_parent[0]);
 			// fetchData();
 		} else if(ret == -1){
 			/* error */
