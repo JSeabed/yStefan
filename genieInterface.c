a05b256d769a60f0fc0d25d56b9a8f7e12992c8b
Jeroen <jeroen@seabed.nl>
2017-10-27 14:13:16 +0200

    1 f

--- genieInterface.c
+++ genieInterface.c
@@ -218,7 +218,7 @@ int main (int argc, char** argv) {
 			read(fd_parent[0], &readBuffer, BUFFSIZE);
 			printf("\n parent: %s", readBuffer);
 			genieWriteStr(1, readBuffer);
-			fflush(readBuffer);
+			flush(readBuffer);
 			// fetchData();
 		} else if(ret == -1){
 			/* error */
