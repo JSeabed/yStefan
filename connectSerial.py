9a58de5e32099956b19f8414dcb16f3dca727b64
Jeroen <jeroen@seabed.nl>
2017-10-27 10:23:09 +0200

    awf

--- connectSerial.py
+++ connectSerial.py
@@ -36,7 +36,7 @@ def trySerial():
 def scanPorts():
     #ports = list(serial.tools.list_ports.comports())
     try:
-        port = list(serial.tools.list_ports.grep("09d7:0100"))[0][1]
+        port = list(serial.tools.list_ports.grep("09d7:0100"))[1][0]
         print port
         logger.debug("port is: " + port)
         return port
