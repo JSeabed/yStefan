860434834c40d374126e6a3c0906cc9eb8b2d82b
stefan <512683@student.inholland.nl>
2017-11-01 10:45:54 +0100

    s

--- genieInterface.c
+++ genieInterface.c
@@ -134,8 +134,7 @@ void initStruct(struct data *newData){
 
 void clearStruct(struct data *newData){
   newData = (struct data){ 0 };
-  newData->ip = '0';
-  
+  strcpy(newData->ip, '0'); 
   // strncpy(newData->&ip , NULL, 1);
   //strncpy(newData->&status , NULL , 1);
   #if DEBUG
