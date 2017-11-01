1e05ee43f415aa48ccca8d47f8c5075c51689937
stefan <512683@student.inholland.nl>
2017-11-01 10:33:52 +0100

    sfasd

--- genieInterface.c
+++ genieInterface.c
@@ -365,8 +365,9 @@ int main (int argc, char** argv) {
 		#endif
 		//struct data Newdata; //TODO replace
 		usleep(WAIT);
-		if(isStructFull(&newData)
+		if(isStructFull(&newData){
 		   sentData(&newData);
+		  }
 		while(genieReplyAvail()) {
 			genieGetReply(&reply);
 			handleEvent(&reply);
