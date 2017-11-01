06930dffa4a29174d95ebd881df9f38c7db2d2d9
stefan <512683@student.inholland.nl>
2017-11-01 11:48:03 +0100

    casc

--- genieInterface.c
+++ genieInterface.c
@@ -143,8 +143,8 @@ void clearStruct(struct data *newData){
   strcpy(newData->heading, str);
   strcpy(newData->rtk, str);
   strcpy(newData->satallite, str);
-  printf("allData = %s\n", newData->newData);
-  printf("allData (int)= %d\n", newData->newData);
+  printf("allData = %s\n", newData);
+  printf("allData (int)= %d\n", newData);
   // strncpy(newData->&ip , NULL, 1);
   //strncpy(newData->&status , NULL , 1);
   //strncpy(newData->position , (char*)'0', 1);
