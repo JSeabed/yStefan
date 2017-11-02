042215194926bcabb25de9d55d97c96ab74d8097
Jeroen <jeroen@seabed.nl>
2017-11-02 16:04:04 +0100

    fd

--- connectSerial.py
+++ connectSerial.py
@@ -12,17 +12,17 @@ class TrySerialExe():
     def getNRCPort():
     #Uncomment als je trySerial wilt vermijden
     #(als er geen connectie gemaakt kan worden met de novatel chip)
-        return __portDefine(__scanPorts())
+        return _portDefine(_scanPorts())
         return trySerial()
 
 
 #Try to obtain port (tty/USBx).
 #Return None if novatel is not found.
 #Return port if dev/ttyUSBx is found.
-    def __trySerial():
+    def _trySerial():
         attempt = 0
         for i in range(10):
-            port = __portDefine(__scanPorts())
+            port = _portDefine(_scanPorts())
             if port is not None:
                 print "USB found"
                 return port
@@ -34,7 +34,7 @@ class TrySerialExe():
             time.sleep(1)
 
 
-    def __scanPorts():
+    def _scanPorts():
         #ports = list(serial.tools.list_ports.comports())
         try:
             port = list(serial.tools.list_ports.grep("09d7:0100"))[1][0]
@@ -47,7 +47,7 @@ class TrySerialExe():
 
 
 #function to define the port the OEM7 is connected
-    def __portDefine(PORT):
+    def _portDefine(PORT):
         try:
             #PORT = "/dev/ttyUSB1"
             #defining the serial port as a contant value
