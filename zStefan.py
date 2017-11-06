c9109f0e08b641b785be5167a104aee8901d59dc
stefan <512683@student.inholland.nl>
2017-11-06 15:28:53 +0100

    fix

--- zStefan.py
+++ zStefan.py
@@ -180,6 +180,9 @@ def readSerial(port):							#reading all the data that is send by the receiver. 
 				data['gphdt'] = word.split(',')							#add INs to the dictionary
 				(data['gphdt'][0]) = (data['gphdt'][0]).split('$')
 				print data['gphdt'][0]
+
+        except Exception as e:
+	    logger.debug(str(e))
 				#data['gphdt'] = True
 			#if("$GPHDT" in rcv):
 			#	split_GPHDT = rcv.split(',')
