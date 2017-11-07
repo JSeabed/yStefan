835a03158a755bbb16cbeafd3aa5a95a9d1f8949
stefan <512683@student.inholland.nl>
2017-11-07 16:16:35 +0100

    fx

--- connectSerial.py
+++ connectSerial.py
@@ -42,7 +42,7 @@ class TrySerialExe(object):
         return "%s with %s" % (self.getBaudrate, self.scanPorts)
 
 #function to define the port the OEM7 is connected
-    def portDefine(PORT):
+    def portDefine(self, PORT):
         try:
             #PORT = "/dev/ttyUSB1"
             #defining the serial port as a contant value
