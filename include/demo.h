6166b25e01c4bb4a500ab8d55d49799c1f59d43d
stefan <512683@student.inholland.nl>
2017-11-08 16:25:02 +0100

    fix

--- include/demo.h
+++ include/demo.h
@@ -1,8 +1,6 @@
 #ifndef _DEMO_HEADER
 #define _DEMO_HEADER
 
-#include "struct.h"
-
 void demo(void);
 void demoRead(int , struct genieReplyStruct);
 /**/
--- src/genieInterface.c
+++ src/genieInterface.c
@@ -16,7 +16,7 @@
     #include <Diablo_Types4D.h>
     #include <Diablo_const4D.h>
 #endif
-#include "demo.h"
+#include "../include/demo.h"
 
 #if GENIE
     #define GENIE_OBJ_FORM 10
