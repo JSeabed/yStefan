3705cb13fe85e9029c310bf6bb9ff8b2dbf5de54
stefan <512683@student.inholland.nl>
2018-04-03 11:59:03 +0200

    fix

--- shutdown.sh
+++ shutdown.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
-#This script is used to shutdown the SGR7 when the powerbutton is pushed.
-#
+#This script is used to initialise the gpio-pins of the Pi. These pins are used for the display. 
+# After the initialisation. The script will read the GPIO pin used for shutdown.
+
+$display_pin1 = 12
+$display_pin2 = 13
+$shutdown_pin = 1691
+
+#Set gpio as output
+gpio mode $display_pin1 out
+gpio mode $display_pin2 out
+
+#Set gpio output high
+gpio write $display_pin1 1
+gpio write $display_pin2 1
+
 
 #TODO: Change pin value
-$pin = 1691
-state = $(gpio read $pin)
-
-if [ $state -eq 1 ]
-    then
-        killall python
-        shutdown now
-fi
+while [ 1 ]
+      do
+	    state = $(gpio read $shutdown_pin)
+	    if [ $state -eq 1 ]
+		then
+		    killall python
+		    shutdown now
+	    fi
+	done
